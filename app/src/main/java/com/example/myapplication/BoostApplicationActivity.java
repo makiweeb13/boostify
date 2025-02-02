05a7d2d759978222e2497913dc67ae69ec21cab6
makiweeb13 <mariamaekiskis13@gmail.com>
2025-02-02 20:18:33 +0800

    initial

--- app/src/main/java/com/example/myapplication/BoostApplicationActivity.java
+++ app/src/main/java/com/example/myapplication/BoostApplicationActivity.java
@@ -3,10 +3,10 @@ package com.example.myapplication;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BoostRequestActivity extends AppCompatActivity {
+public class BoostApplicationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_boost_application);
     }
 }
