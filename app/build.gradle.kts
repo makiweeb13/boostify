4abf0bc666dfd1f49e989ccb50d88856ed4a817f
makiweeb13 <mariamaekiskis13@gmail.com>
2025-05-14 11:54:29 +0800

    Add GamesActivity.java Add GameDetailsActivity.java Add BoostRequestActivity.java Add Signup.java Add Login.java Add activity_boost_request.xml Add activity_game_details.xml Add activity_game_list.xml Add activity_login.xml Add activity_signup.xml Add game_item.xml Add search_view_bg.xml Add firebase-auth to libs.versions.toml Update strings.xml with new strings
    Additional Comment:
    
    Added Search Box in GamesActivity.java
    Added filterGames method in GamesActivity.java
    Added Logout Button in MainActivity.java
    Added search_view_bg.xml in Drawings folder(resources) this is for search box

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 }
 
@@ -49,6 +50,7 @@ dependencies {
     implementation(libs.firebase.database)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
